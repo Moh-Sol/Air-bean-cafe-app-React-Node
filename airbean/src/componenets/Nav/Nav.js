c14b820be0025d6072484c1400fe7c3d5be2ae34
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-24 19:22:57 +0100

    .

--- airbean/src/componenets/Nav/Nav.js
+++ airbean/src/componenets/Nav/Nav.js
@@ -1,18 +1,66 @@
+// import React from 'react';
+// import { Link, NavLink } from 'react-router-dom'
+
+
+
+// const Nav = (props) => {
+
+//     console.log(props)
+
+//     return (
+//         <nav className='nav'>
+
+//             <NavLink to='/menu'>Meny </NavLink>
+//             <NavLink to='/about'>Vårt kaffe </NavLink>
+//         </nav>
+//     )
+// }
+// export default Nav;
+
+
+
+
+
+
+
+
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
+import navStyles from '../Nav/Nav.module.css'
 
+import x from '../../assets/graphics/close.svg'
 
-const Nav = (props) => {
-
-    console.log(props)
+export default function Nav(props) {
 
     return (
-        <nav className='nav'>
+        <div className={navStyles.navBody}>
 
-            <NavLink to='/menu'>Meny </NavLink>
-            <NavLink to='/about'>Vårt kaffe </NavLink>
-        </nav>
+            <div
+                onClick={() => props.toggleNav(false)}
+                className={navStyles.xContainer}>
+                <img
+                    alt=""
+                    className={navStyles.x}
+                    src={x}
+                ></img>
+            </div>
+
+            <nav
+                className={navStyles.nav}>
+                <NavLink
+                    className={navStyles.menyandcoffe}
+                    to='/menu'
+                >Meny </NavLink>
+                <p className={navStyles.rectangle}></p>
+                <NavLink
+                    className={navStyles.menyandcoffe}
+                    to='/about'
+                >Vårt kaffe </NavLink>
+                <p className={navStyles.rectangle}></p>
+            </nav>
+        </div>
+
+
     )
 }
-export default Nav;
