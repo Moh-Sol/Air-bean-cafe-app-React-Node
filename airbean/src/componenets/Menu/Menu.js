09296c9033bcd0ccd4e566a9c55679087528bed0
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-24 15:55:33 +0100

    ..

--- airbean/src/componenets/Menu/Menu.js
+++ airbean/src/componenets/Menu/Menu.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Nav from '../Nav/Nav'
+import Styles from './Menu.module.css'
 
 
 
 
 function Menu() {
-    const [menuData, setMenuData] = useState('');
+    const [menuData, setMenuData] = useState([]);
 
     useEffect(async () => {
         const response = await fetch(`http://localhost:5000/api/beans`);
         const data = await response.json();
-        // console.log(data)
         setMenuData(data.menu)
 
     }, [])
 
+
+  
+
     console.log(menuData)
-
-
-
 
     return (
         <div>
-            {menuData.map((item) => {
-               return (<h1> {item.title}</h1>)
-            })}
-
+            <ul className={Styles.dataLista}>
+                {menuData.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <span>{item.price}</span>
+                            <h6>{item.desc}</h6>
+                        </li>
+                    )})}
+            </ul>
         </div>
     );
 }
