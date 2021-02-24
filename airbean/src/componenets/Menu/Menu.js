7de786d9f4e56e9ec259c5cc444f75015af3cd90
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-02-24 15:59:02 +0100

    Update Menu.js

--- airbean/src/componenets/Menu/Menu.js
+++ airbean/src/componenets/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Nav from '../Nav/Nav'
+import Styles from './Menu.module.css'
 
 
 
@@ -10,25 +11,27 @@ function Menu() {
     useEffect(async () => {
         const response = await fetch(`http://localhost:5000/api/beans`);
         const data = await response.json();
-
         setMenuData(data.menu)
 
     }, [])
+
+
+  
 
     console.log(menuData)
 
     return (
         <div>
-             <ul>
-                {​​​​menuData.map((item) => {​​​​
+            <ul className={Styles.dataLista}>
+                {menuData.map((item) => {
                     return (
                         <li key={item.id}>
-                            <span>{​​​​item.title}​​​​</span>
-                            <span>{​​​​item.price}​​​​</span>
-                            <h6>{​​​​item.desc}​​​​</h6>
+                            <span>{item.title}</span>
+                            <span>{item.price}</span>
+                            <h6>{item.desc}</h6>
                         </li>
-                    )}​​​​)}​​​​
-            </ul> 
+                    )})}
+            </ul>
         </div>
     );
 }
