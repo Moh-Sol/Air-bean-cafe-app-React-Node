334459a3c2999026fdf3574ac2b7341f287ad456
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-24 16:57:10 +0100

    .

--- airbean/src/componenets/Menu/Menu.js
+++ airbean/src/componenets/Menu/Menu.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Nav from '../Nav/Nav'
 import Styles from './Menu.module.css'
 
-
+import add from '../../assets/graphics/add.svg'
+import bag from '../../assets/graphics/bag.svg'
+import navIcon from '../../assets/graphics/navicon.svg'
+import addBeverage from '../../actions/addBeverage'
 
 
 function Menu() {
@@ -19,16 +22,32 @@ function Menu() {
     console.log(menuData)
 
     return (
-        <div>
+        <div className={Styles.menuContainer}>
+            <div className={Styles.headerContainer}>
+                <img className={Styles.navIcon} src={navIcon}></img>
+                <div>
+                    <span> 4</span>
+                    <img className={Styles.imgBag} src={bag}></img>
+                </div>
+
+
+            </div>
+            <h1> Meny</h1>
             <ul className={Styles.dataLista}>
                 {menuData.map((item) => {
                     return (
-                        <li key={item.id}>
-                            <span>{item.title}</span>
-                            <span>{item.price}</span>
-                            <h6>{item.desc}</h6>
+                        <li className={Styles.listItem} key={item.id}>
+                            <div>
+                                <img className={Styles.imgPlus} onClick={()=>addBeverage(item)} src={add}></img>
+                            </div>
+                            <div>
+                                <span>{item.title}</span>
+                                <span>{item.price}</span>
+                                <h6>{item.desc}</h6>
+                            </div>
                         </li>
-                    )})}
+                    )
+                })}
             </ul>
         </div>
     );
