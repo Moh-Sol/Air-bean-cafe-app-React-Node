493f0503818177efa9a5b68e41d4cabda17b84ab
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-02-24 14:30:06 +0100

    Update Menu.js

--- airbean/src/componenets/Menu/Menu.js
+++ airbean/src/componenets/Menu/Menu.js
@@ -3,6 +3,41 @@ import Nav from '../Nav/Nav'
 
 
 
+
+function Menu() {
+    const [menuData, setMenuData] = useState('');
+
+    useEffect(async () => {
+        const response = await fetch(`http://localhost:5000/api/beans`);
+        const data = await response.json();
+        // console.log(data)
+        setMenuData(data.menu)
+
+    }, [])
+
+    console.log(menuData)
+
+
+
+
+    return (
+        <div>
+            {menuData.map((item) => {
+               return (<h1> {item.title}</h1>)
+            })}
+
+        </div>
+    );
+}
+
+export default Menu;
+
+
+/* import React, { useState, useEffect } from 'react';
+import Nav from '../Nav/Nav'
+
+
+
 function Menu() {
     const [menuData, setMenuData] = useState('');
 
@@ -22,7 +57,7 @@ function Menu() {
                return (<h1> {item.title}</h1>)
             })}
 
-          {/*  {menuData.map((item) => {
+            {menuData.map((item) => {
                return (
                     <div key={item.id}> 
                         <h2> {item.title}  {item.price}Kr</h2>
@@ -31,10 +66,10 @@ function Menu() {
                 
                 )
             
-            })}  */}
+            })}  
 
         </section>
     );
 }
 
-export default Menu;
+export default Menu; */
