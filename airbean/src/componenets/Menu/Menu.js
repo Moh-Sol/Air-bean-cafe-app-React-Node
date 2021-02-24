ad819d8264e496a9cb2581ee0158660500ae973d
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-02-24 12:21:52 +0100

    Update Menu.js

--- airbean/src/componenets/Menu/Menu.js
+++ airbean/src/componenets/Menu/Menu.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../Nav/Nav'
+
+
 function Menu() {
+    const [menu, setMenu ] = useState ('')
+
+    useEffect(async () => {
+        const res = await fetch (`http://localhost:5000/api/beans`)
+        const data = await res.json();
+        setMenu(data)
+    },[] )
+
+        
     return (
         <div>
             <Nav />
