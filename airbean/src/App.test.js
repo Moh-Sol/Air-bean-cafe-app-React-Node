fd8385dab52179190932505449d3569ae427c940
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-02-26 14:34:00 +0100

    Update App.test.js

--- airbean/src/App.test.js
+++ airbean/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+/* import App from './App'; */
+import Landing from './componenets/Landing/Landing'
 
-test('renders learn react link', () => {
+
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+
+test('history push', () => {
+  render (<Landing />);
+  const 
+
+})
