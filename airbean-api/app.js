10f89665d2bac6a4a394db29f18b93814044660a
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-26 17:50:43 +0100

    .

--- airbean-api/app.js
+++ airbean-api/app.js
@@ -10,7 +10,4 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/beans', beansRouter)
 
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-=======
 app.listen(PORT, () => console.log(`Server started on port  http://localhost${PORT}`))
