b6875f7a3d469dfee1bfe185106917ba96d18d08
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-03-01 14:25:40 +0100

    .

--- airbean/src/componenets/About/About.test.js
+++ airbean/src/componenets/About/About.test.js
@@ -2,18 +2,23 @@
 
 
 import { render, getByTestId, fireEvent } from '@testing-library/react';
-import {createStore} from 'redux';
-import {createStore} from 'react-redux';
+// import {createStore} from 'redux';
+// import {createStore} from 'react-redux';
 import About from './About'
 
-it('h1 visar Vårt kaffe ', () => {
-    const { container } = render(<About />);
+// it('h1 visar Vårt kaffe ', () => {
+//     const { container } = render(<About />);
 
-    const h1Value = getByTestId(container, 'h1-cafe')
+//     const h1Value = getByTestId(container, 'h1-cafe')
 
-    expect(h1Value.textContent).toBe('Vårt kaffe')
+//     expect(h1Value.textContent).toBe('Vårt kaffe')
 
-});
+// });
 
 
 
+test ('right text' , () => {​​​​
+    const rendered = render(<About />)
+    expect(rendered.getByText('Vårt kaffe'));
+    
+    }​​​​)
